71ea0f6a775db83be99e202c54b54019b9067ef6
Jie Li <jl3a12@soton.ac.uk>
2019-06-25 17:55:39 +0100

    response

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const http = require("http");
+const { createServer} = require("http");
+const { createReadStream} = require("fs");
+const sendFile = (res, status, type, filePath) => {
+    res.writeHead(status, {"Content-Type" : type});
 
-http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"});
+    createReadStream(filePath).pipe(res);
+};
 
-    res.end("hello iris");
-})
-.listen(3000);
+createServer((req, res)=> {
+
+    switch(req.url) {
+        case "/": 
+            return sendFile(res, 200, "text/html", "./index.html");
+        case "/search/car/car1":
+            return sendFile(res, 200, "image/jpeg", "https://live.staticflickr.com/65535/48127193876_303c27f13a_m.jpg");
+        case "/styles.css":
+            return sendFile(res, 200, "text/css", "./styles.css");
+        default:
+            return sendFile(res, 200, "text/html", "./404.html");    
+    }
+
+}).listen(3000);
+
+console.log("Iris's personal website running on port 3000");
